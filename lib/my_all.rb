0bbe4bc7f8eda9e43ec42396aa537b8ef92c74b6
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-05-14 13:16:32 +0800

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i=0
+  ans=[]
+  while i<collection.length
+    ans << yield(collection[i])
+    i+=1
+  end
+  if ans.include?(false)
+    false
+  else
+    true
+  end
 end
